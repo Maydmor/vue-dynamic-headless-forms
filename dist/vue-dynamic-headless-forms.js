b1d12829f1cdf350739291004a8ffb4fa75cb248
Jakob Leibetseder <jakob.leibetseder@lec.tugraz.at>
2024-11-27 12:22:47 +0100

    new build with debug message

--- dist/vue-dynamic-headless-forms.js
+++ dist/vue-dynamic-headless-forms.js
@@ -1,7 +1,7 @@
 import { defineComponent as p, mergeModels as x, useModel as M, toRefs as T, computed as i, onMounted as V, renderSlot as _, unref as n } from "vue";
 import { helpers as a, required as q, requiredIf as w, requiredUnless as F, minLength as y, maxLength as D, maxValue as L, minValue as R, sameAs as $ } from "@vuelidate/validators";
-import I from "@vuelidate/core";
-const b = /* @__PURE__ */ p({
+import A from "@vuelidate/core";
+const U = /* @__PURE__ */ p({
   __name: "DynamicFormField",
   props: /* @__PURE__ */ x({
     field: {},
@@ -19,7 +19,7 @@ const b = /* @__PURE__ */ p({
     const h = i(() => {
       const r = {};
       for (const e of t.value.validationRules || [])
-        e.name == "required" && (r.required = a.withMessage(e.errorText, q)), e.name == "required_if" && (r.requiredIf = a.withMessage(e.errorText, w(!!l.value[e.otherFieldName]))), e.name == "required_unless" && (r.requiredUnless = a.withMessage(e.errorText, F(!!l.value[e.otherFieldName]))), e.name == "min_length" && (r.minLength = a.withMessage(e.errorText, y(l.value[e.length]))), e.name == "max_length" && (r.maxLength = a.withMessage(e.errorText, D(l.value[e.length]))), e.name == "less_than" && (r.lessThan = a.withMessage(e.errorText, L(l.value[e.value]))), e.name == "greater_than" && (r.greaterThan = a.withMessage(e.errorText, R(l.value[e.value]))), e.name == "same_as" && (r.sameAs = a.withMessage(e.errorText, $(l.value[e.otherFieldName])));
+        console.debug("Add validation rule: ", e), e.name == "required" && (r.required = a.withMessage(e.errorText, q)), e.name == "required_if" && (r.requiredIf = a.withMessage(e.errorText, w(!!l.value[e.otherFieldName]))), e.name == "required_unless" && (r.requiredUnless = a.withMessage(e.errorText, F(!!l.value[e.otherFieldName]))), e.name == "min_length" && (r.minLength = a.withMessage(e.errorText, y(l.value[e.length]))), e.name == "max_length" && (r.maxLength = a.withMessage(e.errorText, D(l.value[e.length]))), e.name == "less_than" && (r.lessThan = a.withMessage(e.errorText, L(l.value[e.value]))), e.name == "greater_than" && (r.greaterThan = a.withMessage(e.errorText, R(l.value[e.value]))), e.name == "same_as" && (r.sameAs = a.withMessage(e.errorText, $(l.value[e.otherFieldName])));
       return r;
     }), c = i(() => ({
       model: o.value
@@ -37,7 +37,7 @@ const b = /* @__PURE__ */ p({
       }
       return t.value.default;
     }
-    const d = I(g, c);
+    const d = A(g, c);
     return V(() => {
       t.value.default && (o.value = t.value.default);
     }), (r, e) => _(r.$slots, n(t).type, {
@@ -50,5 +50,5 @@ const b = /* @__PURE__ */ p({
   }
 });
 export {
-  b as DynamicFormField
+  U as DynamicFormField
 };
